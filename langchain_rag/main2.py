54c38c0b1f7896a0899f8c81dbc361fc56afd4a3
thee <theekshana.sandaru@gmail.com>
2025-11-27 19:22:08 +0100

    rag basics

---
+++ langchain_rag/main2.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from injestion import qdrant_vector_store
+
+from langchain_classic import hub
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+load_dotenv()
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+if __name__ == "__main__":
+    print("Retreaving...")
+
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI()
+
+    retreaval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    template = """
+    Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Always say "thanks for asking!" at the end of the answer
+
+    {context}
+
+    Question: {question}
+    Helpful Answer:
+    """
+
+    custom_rag_promt = PromptTemplate.from_template(template)
+    # combin_docs_chain = create_stuff_documents_chain(
+    #     llm, retreaval_qa_chat_prompt)
+    # retraval_chain = create_retrieval_chain(
+    #     retriever=qdrant_vector_store.as_retriever(), combine_docs_chain=combin_docs_chain
+    # )
+    rag_chain = (
+        {"context": qdrant_vector_store.as_retriever() | format_docs,
+         "question": RunnablePassthrough()}
+        | custom_rag_promt
+        | llm
+    )
+
+    result = rag_chain.invoke(
+        "What is Pinecone?"
+    )
+
+    print(result)
---
+++ langchain_rag/using_pdf/main.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_classic.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain_classic.vectorstores import FAISS
+from langchain_classic import hub
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("Hi")
+    loader = PyPDFLoader("langchain_rag/using_pdf/react.pdf")
+    content = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=30
+    )
+
+    docs = text_splitter.split_documents(content)
+    print(docs)
+    embeddings = OpenAIEmbeddings()
+
+    vector_store = FAISS.from_documents(
+        docs, embeddings
+    )
+    vector_store.save_local("faiss_index_react")
+
+    new_vectorstore = FAISS.load_local(
+        "faiss_index_react", embeddings, allow_dangerous_deserialization=True
+    )
+
+    retreaval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combin_docs_chain = create_stuff_documents_chain(
+        OpenAI(), retreaval_qa_chat_prompt
+    )
+
+    retreival_chain = create_retrieval_chain(
+        new_vectorstore.as_retriever(), combin_docs_chain
+    )
+
+    res = retreival_chain.invoke({
+        "input": "Give me the gist of ReAct in 3 sentences"
+    })
+    print(res["answer"])
