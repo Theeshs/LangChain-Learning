80e430325f038d7dbad4b6525526d2c585a299b8
thee <theekshana.sandaru@gmail.com>
2025-12-04 22:55:49 +0100

    updated

---
+++ langgraph-corrective-rag/injestion.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+doc_list = [item for sublist in docs for item in sublist]
+
+text_spitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+
+doc_splits = text_spitter.split_documents(doc_list)
+
+# vectorstore = Chroma.from_documents(
+#     documents=doc_splits,
+#     collection_name="rag-chroma",
+#     embedding=OpenAIEmbeddings(),
+#     persist_directory="./.chroma"
+# )
+
+retriver = Chroma(
+    collection_name="rag-chroma",
+    embedding_function=OpenAIEmbeddings(),
+    persist_directory="./.chroma"
+).as_retriever()
