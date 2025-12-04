f139f2cea157c93e9c855ccbf83b572b14852f6b
thee <theekshana.sandaru@gmail.com>
2025-12-04 23:07:14 +0100

    structured output grader

---
+++ langgraph-corrective-rag/graph/nodes/retrieve.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict
+
+from graph.state import GraphState
+from injestion import retriver
+
+
+def retrive(state: GraphState) -> Dict[str, Any]:
+    print("Retrive")
+    question = state["question"]
+
+    documents = retriver.invoke(question)
+    return {"documents": documents, "question": question}
