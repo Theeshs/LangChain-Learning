ded49ccdfd74584502a264f3e140594118582efe
thee <theekshana.sandaru@gmail.com>
2025-12-05 21:44:54 +0100

    updated

--- langgraph_corrective_rag/main.py
+++ langgraph_corrective_rag/main.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("Hello..! from Corrective Rag")
-    # print(app.invoke(input={"question": "what is agent memory"}))
+    print("Asking question that related to things in vector store")
+    print(app.invoke(input={"question": "what is agent memory"}))
+    print("Asking a question based on a context that is not available in vector store")
     print(app.invoke(input={"question": "How to make pizza"}))
